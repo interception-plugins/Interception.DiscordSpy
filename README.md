b937047ae9ac970b885a07e568fa1b84a1233435
wristcry <cryingkiddo@gmail.com>
2025-06-14 12:49:23 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Interception.DiscordSpy
+
+Unturned RocketMod plugin that sends a result screenshot of /spy command to your Discord text channel via webhook
+	
+Part of [Interception.Module](https://github.com/interception-plugins/Interception.Module) example plugins
+
+## Configuration
+
+- webhook_url
+A link to your webhook (text channel settings -> Intergrations -> Create Webhook -> Copy Webhook URL)
+- webhook_settings
+Webhook settings
+	- username
+	Custom username of webhook
+	Remove this parameter if you want to keep default username
+	- avatar_url
+	Custom avatar of webhook
+	Remove this parameter if you want to keep default avatar
+	- flags
+	Message flags
---
+++ main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Steamworks;
+
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using Rocket.API.Collections;
+
+using interception.enums;
+using interception.discord;
+using interception.discord.types;
+using interception.serialization.types.discord;
+
+namespace interception.plugins.discordspy {   
+    public class spy_player_component : UnturnedPlayerComponent {
+        void on_spy_ready(CSteamID steamID, byte[] data) {
+            var wh = (webhook)main.cfg.webhook_settings;
+            wh.content = main.instance.Translate("on_spy", base.Player.CharacterName, base.Player.CSteamID.m_SteamID);
+            wh.add_file(data, $"spy_{steamID.m_SteamID}.png");
+            webhook_manager.send_webhook_async(main.cfg.webhook_url, wh);
+        }
+
+        protected override void Load() {
+            base.Player.Player.onPlayerSpyReady += on_spy_ready;
+        }
+
+        protected override void Unload() {
+            base.Player.Player.onPlayerSpyReady -= on_spy_ready;
+        }
+    }
+    
+    public class config : IRocketPluginConfiguration, IDefaultable {
+        public string webhook_url;
+        public s_webhook webhook_settings;
+       
+        public void LoadDefaults() {
+            webhook_url = "0_o";
+            webhook_settings = new s_webhook() { 
+                username = "Interpception's Unturned Spy Bot",
+                avatar_url = "https://avatars.akamai.steamstatic.com/08c9944b3176faed9e762311495d14e2860a538a_full.jpg",
+                flags = new List<e_webhook_flag>() { 
+                    e_webhook_flag.silent
+                }
+            };
+        }
+    }
+
+    public class main : RocketPlugin<config> {
+        internal static main instance;
+        internal static config cfg;
+
+        protected override void Load() {
+            instance = this;
+            cfg = instance.Configuration.Instance;
+            GC.Collect();
+        }
+
+        protected override void Unload() {
+            cfg = null;
+            instance = null;
+            GC.Collect();
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList
+        {
+            { "on_spy", "Screenshot for player \"{0}\" [{1}]" }
+        };
+    }
+}
+
